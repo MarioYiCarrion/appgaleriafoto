1cf93209422b9af066e2a271594b1cf0542d5df8
MarioYiCarrion <MarioYiCarrion@hotmail.com>
2024-03-06 22:23:31 -0500

    Segunda Carga

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,10 @@ android {
         jvmTarget = "1.8"
     }
 
+    viewBinding{
+        enable = true
+    }
+
 
 }
 
--- app/src/main/java/pe/edu/idat/appgaleriafoto/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/appgaleriafoto/MainActivity.kt
@@ -1,11 +1,51 @@
 package pe.edu.idat.appgaleriafoto
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import pe.edu.idat.appgaleriafoto.databinding.ActivityMainBinding
+import java.io.File
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var archivo: File
+    private var rutaFotoActual = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btntomarfoto.setOnClickListener(this)
+        binding.btncompartirfoto.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.btntomarfoto -> tomarfoto()
+            R.id.btncompartirfoto -> compartirfoto()
+        }
+    }
+
+    private fun compartirfoto() {
+
+    }
+
+    private fun tomarfoto() {
+        abrirCamara.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+
+    }
+    private val abrirCamara = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        resultado ->
+        if(resultado.resultCode == RESULT_OK){
+            val foto = resultado.data!!
+            val fotoBitMap = foto.extras!!.get("data") as Bitmap
+            binding.ivfoto.setImageBitmap(fotoBitMap)
+        }
     }
 }
